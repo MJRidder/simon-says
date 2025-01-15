abc094625805560f53df45b10f4a37e8026f1e22
Maarten Ridder <maarten_ridder@hotmail.com>
2025-01-15 15:22:55 +0000

    added addTurn and tests

--- scripts/game.js
+++ scripts/game.js
@@ -16,11 +16,18 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    //showTurns();
+    //showTurn();
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn };
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
